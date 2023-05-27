b3a8a3b393bd207667cff842de768db19a947792
Matt <matt@spectrebot.net>
2023-05-27 10:18:18 +0100

    fix bugs with responding to replies

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,15 @@
 const { Events } = require('discord.js');
 
+// Require dotenv to load the .env file
+const dotenv = require('dotenv');
+
+// Load the .env file
+dotenv.config();
+
 console.log('called messageCreate.js')
 
 const cooldown = new Map();
 const cooldownTime = 3600;
-const date = new Date();
 
 module.exports =  {
 	name: Events.MessageCreate,
@@ -16,23 +21,23 @@ module.exports =  {
 		// Define cooldownIsClean
 		const cooldownIsClean = Math.floor(cooldown.get(message.guild.id)) < Math.floor(message.createdTimestamp/1000);
 
-		if (!message.author.bot && message.mentions.has('1087809161275916489') && cooldownIsClean) {
+		if (!message.author.bot && message.mentions.has(`${process.env.CLIENT_ID}`) && cooldownIsClean && message.type !== 19) {
 
-			const newCooldown = Math.floor(date.getTime()/1000) + cooldownTime;
+			const newCooldown = Math.floor(message.createdTimestamp/1000) + cooldownTime;
 			console.log("new ping in\n channel: #" + message.channel.name + '\n guild: ' + message.guild.name + '\n from user: ' + message.author.username + '#' + message.author.discriminator + '\n message: ' + message.content)
 			message.reply({
-				content: `${message.author} pinged <@&891299458850103347>!\n\nDead Chat Ping is now disabled until <t:${newCooldown}:R>`,
+				content: `${message.author} pinged <@&891299458850103347>!\n\nDead Chat Ping is now disabled until <t:${newCooldown}:t>`,
 			});
 			cooldown.set(message.guild.id, newCooldown);
 
-		} else if(!message.author.bot && message.mentions.has('1087809161275916489') && !cooldownIsClean) {
+		} else if(!message.author.bot && message.mentions.has(`${process.env.CLIENT_ID}`) && !cooldownIsClean && message.type !== 19) {
 
 			message.reply('Dead Chat Ping was sent recently, you can next ping this role <t:' + cooldown.get(message.guild.id) + ':R>');
 
 		} else {
 
 			return;
-			
+
 		}
 	},
 };
